8813e5ba5cc2df795b824023eae4b92359eef725
Jimmy Ou <jimmy.ou@oocl.com>
2019-08-17 13:33:48 +0800

    [jimmy] complete test generate report

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -2,13 +2,49 @@ package sales;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
 public class SalesAppTest {
 
 	@Test
 	public void testGenerateReport() {
-		
-		SalesApp salesApp = new SalesApp();
+		String salesId = "DUMMY";
+		Long oneDay = 24L * 60 * 60 * 1000;
+		Date now = new Date(0);
+		Date tomorrow = new Date(now.getTime() + oneDay);
+
+		Sales mockedSales = spy(Sales.class);
+		when(mockedSales.getEffectiveFrom()).thenReturn(now);
+		when(mockedSales.getEffectiveTo()).thenReturn(tomorrow);
+
+		SalesDao mockedSalesDao = mock(SalesDao.class);
+		when(mockedSalesDao.getSalesBySalesId(salesId)).thenReturn(mockedSales);
+
+		SalesReportData mockSalesReportData = spy(new SalesReportData());
+		mockSalesReportData.setConfidential(false);
+		when(mockSalesReportData.getType()).thenReturn("SalesActivity");
+		List<SalesReportData> mockSalesReportDataList = Arrays.asList(mockSalesReportData);
+
+		SalesReportDao mockSaleReportDao = mock(SalesReportDao.class);
+		when(mockSaleReportDao.getReportData(mockedSales)).thenReturn(mockSalesReportDataList);
+
+		EcmService mockedEcmService = mock(EcmService.class);
+
+		SalesActivityReport mockedReport = mock(SalesActivityReport.class);
+		when(mockedReport.toXml()).thenReturn("HAHA I am mocking");
+
+		SalesApp salesApp = spy(new SalesApp());
+		when(salesApp.generateSalesDao()).thenReturn(mockedSalesDao);
+		when(salesApp.generateSalesReportDao()).thenReturn(mockSaleReportDao);
+		when(salesApp.generateEcmService()).thenReturn(mockedEcmService);
+		when(salesApp.generateReport(any(), any())).thenReturn(mockedReport);
+
 		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
+
+		verify(mockedEcmService).uploadDocument("HAHA I am mocking");
 	}
 }
