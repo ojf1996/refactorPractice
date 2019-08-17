847bbadf8bda1f9e2efb7ddf3f64b2614d88c927
Jimmy Ou <jimmy.ou@oocl.com>
2019-08-17 13:57:36 +0800

    [jimmy] [1]fixbug: wrong date writen in test completed before, though which not cause the test case fail. [2] complete test when date is not effective, which shuold not invoke function left.

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -18,8 +18,8 @@ public class SalesAppTest {
 		Date tomorrow = new Date(now.getTime() + oneDay);
 
 		Sales mockedSales = spy(Sales.class);
-		when(mockedSales.getEffectiveFrom()).thenReturn(now);
-		when(mockedSales.getEffectiveTo()).thenReturn(tomorrow);
+		when(mockedSales.getEffectiveTo()).thenReturn(now);
+		when(mockedSales.getEffectiveFrom()).thenReturn(tomorrow);
 
 		SalesDao mockedSalesDao = mock(SalesDao.class);
 		when(mockedSalesDao.getSalesBySalesId(salesId)).thenReturn(mockedSales);
@@ -56,8 +56,8 @@ public class SalesAppTest {
 		Date tomorrow = new Date(now.getTime() + oneDay);
 
 		Sales mockedSales = spy(Sales.class);
-		when(mockedSales.getEffectiveFrom()).thenReturn(now);
-		when(mockedSales.getEffectiveTo()).thenReturn(tomorrow);
+		when(mockedSales.getEffectiveTo()).thenReturn(now);
+		when(mockedSales.getEffectiveFrom()).thenReturn(tomorrow);
 
 		SalesDao mockedSalesDao = mock(SalesDao.class);
 		when(mockedSalesDao.getSalesBySalesId(salesId)).thenReturn(mockedSales);
@@ -87,4 +87,44 @@ public class SalesAppTest {
 		verify(mockedSalesDao, times(0)).getSalesBySalesId(any());
 		verify(mockedEcmService, times(0)).uploadDocument("HAHA I am mocking");
 	}
+
+    @Test
+    public void should_return_before_invoke_other_function_when_date_is_effective() {
+        String salesId = "DUMMY";
+        Long oneDay = 24L * 60 * 60 * 1000;
+        Date now = new Date();
+        Date tomorrow = new Date(now.getTime() + oneDay);
+
+        Sales mockedSales = spy(Sales.class);
+        when(mockedSales.getEffectiveTo()).thenReturn(tomorrow);
+		when(mockedSales.getEffectiveFrom()).thenReturn(new Date(0));
+
+        SalesDao mockedSalesDao = mock(SalesDao.class);
+        when(mockedSalesDao.getSalesBySalesId(salesId)).thenReturn(mockedSales);
+
+        SalesReportData mockSalesReportData = spy(new SalesReportData());
+        mockSalesReportData.setConfidential(false);
+        when(mockSalesReportData.getType()).thenReturn("SalesActivity");
+        List<SalesReportData> mockSalesReportDataList = Arrays.asList(mockSalesReportData);
+
+        SalesReportDao mockSaleReportDao = mock(SalesReportDao.class);
+        when(mockSaleReportDao.getReportData(mockedSales)).thenReturn(mockSalesReportDataList);
+
+        EcmService mockedEcmService = mock(EcmService.class);
+
+        SalesActivityReport mockedReport = mock(SalesActivityReport.class);
+        when(mockedReport.toXml()).thenReturn("HAHA I am mocking");
+
+        SalesApp salesApp = spy(new SalesApp());
+        when(salesApp.generateSalesDao()).thenReturn(mockedSalesDao);
+        when(salesApp.generateSalesReportDao()).thenReturn(mockSaleReportDao);
+        when(salesApp.generateEcmService()).thenReturn(mockedEcmService);
+        when(salesApp.generateReport(any(), any())).thenReturn(mockedReport);
+
+        salesApp.generateSalesActivityReport(salesId, 1000, false, false);
+
+        verify(mockedSalesDao).getSalesBySalesId(any());
+        verify(mockSaleReportDao, times(0)).getReportData(mockedSales);
+        verify(mockedEcmService, times(0)).uploadDocument("HAHA I am mocking");
+    }
 }
